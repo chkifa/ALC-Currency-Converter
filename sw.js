b153714b97d4584e62c5574344ba2b3007518a83
RUFAI YUSUF ZAKARI <rufaig6@gmail.com>
2018-06-30 14:29:22 +0100

    ALC 3.0rufaig6@gmail

---
+++ sw.js
@@ -0,0 +1,45 @@
+
+// on install state
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(appCacheName).then(function(cache){
+			return cache.addAll(appCacheAssets);
+		})
+	);
+});
+
+// on activate state
+self.addEventListener('activate', function(event){
+	event.waitUntil(
+		caches.keys().then(function(cacheNames){
+			return Promise.all(
+				cacheNames.filter(function(cacheName){
+					return cacheName.startsWith('wnes-') && cacheName !== appCacheName;
+				}).map(function(cacheName){
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+});
+
+
+self.addEventListener('fetch', function(event){
+	// event.respondWith('hello');
+	// console.log('hello');
+	event.respondWith(
+		caches.match(event.request).then(function(response){
+			if(response){
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
+});
+
+// on message
+self.addEventListener('message', function(event){
+	if(event.data.action == 'skipWaiting'){
+		self.skipWaiting();
+	}
+});
